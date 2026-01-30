65a38eb96a05853c9a86fc4c3fb642097ac1ab83
Jusk89 <asan.zhusup@gmail.com>
2026-01-30 11:10:42 +0500

    Numbers ex

--- Numbers.py
+++ Numbers.py
@@ -1,10 +1,20 @@
-#To change the value of a global variable inside a function, refer to the variable by using the global keyword:
-x = "awesome"
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
 
-def myfunc():
-  global x
-  x = "fantastic"
+#convert from int to float:
+a = float(x)
 
-myfunc()
+#convert from float to int:
+b = int(y)
 
-print("Python is " + x)
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
