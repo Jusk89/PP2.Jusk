175c9e51e75e4ab027eb4f8e043f9216a86fc7af
Jusk89 <asan.zhusup@gmail.com>
2026-01-30 10:54:23 +0500

    Data types

--- Data_Types.py
+++ Data_Types.py
@@ -3,3 +3,11 @@ x, y, z = fruits
 print(x)
 print(y)
 print(z)
+#Text Type:	str
+#Numeric Types:	int, float, complex
+#Sequence Types:	list, tuple, range
+#Mapping Type:	dict
+#Set Types:	set, frozenset
+#Boolean Type:	bool
+#Binary Types:	bytes, bytearray, memoryview
+#None Type:	NoneType
