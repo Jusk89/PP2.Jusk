bb91701fc0a08fde191b156f1ea6fe3f1d1eafb0
Jusk89 <asan.zhusup@gmail.com>
2026-02-06 01:11:07 +0500

    For

---
+++ Practice 02/loops/for_loop.py
@@ -0,0 +1,26 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+#2
+"""for x in "banana":
+  print(x)"""
+
+#3
+"""for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")"""
+
+"""for x in range(6):
+  if x == 3: break
+  print(x)
+else:
+  print("Finally finished!")"""
+
+"""adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)"""
