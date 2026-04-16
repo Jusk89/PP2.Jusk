ae7154b6da4f2690dc2aae7885114714f10dc38d
Jusk89 <asan.zhusup@gmail.com>
2026-04-17 01:27:52 +0500

    Add Practice7 - Pygame games: Mickey's clock, music player, moving ball

--- Practice 09/moving_ball/ball.py
+++ Practice 09/moving_ball/ball.py
@@ -6,8 +6,10 @@ screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 surface = pygame.Surface((100, 100), pygame.SRCALPHA)
 surface.fill((255, 0, 0, 128))
 pygame.display.set_caption("Моё первое окно")
-x= 30
-y= 30
+x= 50
+y= 50
+radius = 25
+speed = 9
 running = True
 is_blue= True
 
@@ -25,15 +27,26 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
     key=pygame.key.get_pressed()
-    if key[pygame.K_w]: y -=9
-    if key[pygame.K_s]: y +=9
-    if key[pygame.K_d]: x +=9
-    if key[pygame.K_a]: x -=9
+    if key[pygame.K_w]:
+        if y - radius - speed >= 0:
+            y -= speed
+
+    if key[pygame.K_s]:
+        if y + radius + speed <= screen.get_height():
+            y += speed
+
+    if key[pygame.K_d]:
+        if x + radius + speed <= screen.get_width():
+            x += speed
+
+    if key[pygame.K_a]:
+        if x - radius - speed >= 0:
+            x -= speed
     screen.fill((0, 0, 0))
 
     if is_blue: color = (0, 128, 255)
     else: color = (255, 100, 0)
-    pygame.draw.rect(screen, color, pygame.Rect(x, y, 20, 20))
+    pygame.draw.circle(screen, color, (x, y), radius)
     screen.blit(surface, (50, 50))
     clock.tick(60)
     pygame.display.flip()
