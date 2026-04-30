8c1d1fc4fd40d6717f1cab9a636bb6b1ec5015f9
Jusk89 <asan.zhusup@gmail.com>
2026-05-01 03:49:49 +0500

    fir

--- TSIS3/persistence.py
+++ TSIS3/persistence.py
@@ -7,22 +7,26 @@ LEADERBOARD_FILE = "leaderboard.json"
 
 def load_json(filename, default):
     if os.path.exists(filename):
-        with open(filename, "r") as f:
-            return json.load(f)
+        with open(filename, "r") as file:
+            return json.load(file)
     return default
 
 
 def save_json(filename, data):
-    with open(filename, "w") as f:
-        json.dump(data, f, indent=4)
+    with open(filename, "w") as file:
+        json.dump(data, file, indent=4)
 
 
-def save_score(leaderboard, name, score, distance):
+def save_score(name, score, distance):
+    leaderboard = load_json(LEADERBOARD_FILE, [])
+
     leaderboard.append({
         "name": name,
         "score": score,
         "distance": int(distance)
     })
 
-    leaderboard.sort(key=lambda x: x["score"], reverse=True)
-    return leaderboard[:10]
+    leaderboard.sort(key=lambda item: item["score"], reverse=True)
+    leaderboard = leaderboard[:10]
+
+    save_json(LEADERBOARD_FILE, leaderboard)
